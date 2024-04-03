9e633ecc26247f99f71b2c8abefb09b33e0b2448
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-04-02 20:19:10 -0500

    Mi segundo cambio

--- index.js
+++ index.js
@@ -2,14 +2,65 @@ const express = require('express');
 const app = express();
 
 const productos = [
-    { id: 1, nombre: 'Producto 1', precio: 10 },
-    { id: 2, nombre: 'Producto 2', precio: 20 },
-    { id: 3, nombre: 'Producto 3', precio: 30 }
+    { id: 0, nombre: 'Producto 1', precio: 10 },
+    { id: 1, nombre: 'Producto 2', precio: 20 },
+    { id: 2, nombre: 'Producto 3', precio: 30 }
   ];
 
 app.get('/productos', (req, res) => {
-res.json(productos);
+  res.json(productos);
 });
+
+app.post('/productos', (req,res) => {
+    let body= "", data;
+
+    req.on("data", chunck => { body += chunck.toString(); });
+    
+    req.on("end", () => {
+        try {
+            data = JSON.parse(body);
+        } catch (error) {
+            res.json({
+                "message": "Error - Procesamiento de los datos"
+            });
+        }
+
+        productos.push(data);
+        res.json(productos);
+    });
+})
+
+app.put('/productos/upd/:id', (req,res) => {
+    let body= "", data;
+
+    req.on("data", chunck => { body += chunck.toString(); });
+    
+    req.on("end", () => {
+        try {
+            data = JSON.parse(body);
+        } catch (error) {
+            res.json({
+                "message": "Error - Procesamiento de los datos",
+                "addition": `${error}`
+            });
+        }
+
+        productos[req.params.id] = data;
+        res.json(productos);
+    });
+})
+
+app.delete('/productos/del/:id', (req,res) => {
+    try {
+        productos.splice(req.params.id,1);
+        res.json(productos);
+    } catch (error) {
+        res.json({
+            "message": "Error - Procesamiento de los datos",
+            "addition": `${error}`
+        });
+    }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
